2ad24c33507701e448a3f1562901e58fe86621ae
Kim Da Eun <midekuna@gmail.com>
2023-10-27 01:16:14 +0900

    ⚡️ 코드 분할로 번들 사이즈 축소
    Lighthouse 점수 92점 -> 98점
    - lazy와 Suspense를 사용해 프리뷰 페이지와 상세 보기 페이지를 동적 import하여 번들 사이즈를 축소

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+// 정적 import
+// import ListPage from './pages/ListPage/index'
+// import ViewPage from './pages/ViewPage/index'
+
+// 동적 import(런타임에 모듈을 임포트하도록 함)
+const ListPage = lazy(() => import('./pages/ListPage/index'));
+const ViewPage = lazy(() => import('./pages/ViewPage/index'));
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
-      </Switch>
+      {/* Suspense로 동적 import한 컴포넌트를 감싸지 않으면 컴포넌트가 제대로 로드 되지 않는 순간이 생긴다 */}
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Switch>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </Switch>
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
